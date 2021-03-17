b43e85d755c0c33a2d45a83f60d3c48a4bb21eff
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-17 11:07:59 +0100

    Add controls one object

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -7,3 +7,11 @@ exports.getAllProducts = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+exports.getProduct = async (req, res) => {
+  try {
+    res.status(200).json(await Product.find({ _id: req.params.id }));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- models/product.model.js
+++ models/product.model.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
-  // _id: { type: Object, required: true },
+  _id: { type: Object, required: true },
+  description: { type: String, required: true },
+  img: { type: String, required: true },
   name: { type: String, required: true },
-  // description: { type: String, required: true },
-  // img: { type: String, required: true },
-  // tag: { type: String, required: true },
-  // price: { type: Number, required: true },
-  // quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  tag: { type: String, required: true },
+  quantity: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("Product", Product);
